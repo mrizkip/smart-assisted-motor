932fe88a6955b1b9b1566daf915bda4d7a20858e
QIDONK\mrizk_000 <mrizkip@hotmail.com>
2018-03-28 14:20:29 +0700

    all view

---
+++ app/src/main/java/com/hanyasoftware/android/smartassistedmotor/DetailGuestActivity.java
@@ -0,0 +1,13 @@
+package com.hanyasoftware.android.smartassistedmotor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailGuestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_guest);
+    }
+}
